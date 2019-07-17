084b5c2b233a237de28aeb45cac3884bc2bc7d25
安小游 <471607346@qq.com>
2019-07-17 18:07:27 +0800

    -m

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$http = new swoole_http_server('0.0.0.0',8011);
+$http->set([
+    'enable_static_handler' => true,
+    'document_root' => '/Users/yinchuanjiang/www/swoole/apidoc',
+    'worker_num' => 8,    //开启的进程数  cup的核数 1-4倍
+    'max_request' => 10000,
+]);
+$http->on('request',function ($request,$response){
+    $str = "Say ";
+    $timer_id = swoole_timer_tick( 1000 , function($timer_id , $params) use ($str) {
+        echo $str . $params;  // 输出“Say Hello”
+
+    } , "Hello" );
+    $response->end(json_encode($request->get));
+});
+$http->start();
